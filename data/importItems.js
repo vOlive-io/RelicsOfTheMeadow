6a6a37daa0ef8533bb9e3ffc9c5af462031814a1
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-11-13 11:26:46 -0700

    Update importItems.js

--- data/importItems.js
+++ data/importItems.js
@@ -1,23 +1,101 @@
 export const importItems = [
+  /////////////////
+  //  HAPPINESS  //
+  /////////////////
+  {
+    name: "Balloons",
+    price: 1,
+    statBoosts: {happiness: 1},
+  },
+  {
+    name: "Plushies",
+    price: 2,
+    statBoosts: {happiness: 2},
+  },
   {
     name: "Cookies",
     price: 3,
-    statBoosts: {happiness: 2},
+    statBoosts: {happiness: 3},
+  },
+  {
+    name: "Road Paving Supplies",
+    price: 5,
+    statBoosts: {happiness: 5},
+  },
+  /////////////////
+  //  PROTECTION //
+  /////////////////
+  {
+    name: "Wooden Fence Supplies",
+    price: 3,
+    statBoosts: {protection: 1},
+  },
+  {
+    name: "Stone Fence Supplies",
+    price: 4,
+    statBoosts: {protection: 2},
+  },
+  {
+    name: "Reinforced Fence Supplies",
+    price: 5,
+    statBoosts: {protection: 3},
+  },
+  /////////////////
+  //    TROOPS   //
+  /////////////////
+  {
+    name: "Crossbows",
+    price: 8,
+    statBoosts: {troops: 1},
   },
   {
     name: "Swords",
     price: 10,
-    statBoosts: {troops: 1},
+    statBoosts: {troops: 2},
   },
+   {
+    name: "Arrows Rockets",
+    price: 15,
+    statBoosts: {troops: 3},
+  },
+  {
+    name: "Catapults",
+    price: 20,
+    statBoosts: {troops: 4},
+  },
+  {
+    name: "Siege Supplies",
+    price: 20,
+    statBoosts: {troops: 5},
+  },
+  /////////////////
+  //    ENERGY   //
+  /////////////////
   {
     name: "Energy Drink",
     price: 3,
-    statBoosts: {happiness: 1, energy: 5},
+    statBoosts: {energy: 5},
   },
+  /////////////////
+  //     GOLD    //
+  /////////////////
   {
     name: "Raw gold",
-    price: 50,
-    statBoosts: {gold: 10},
+    price: 30,
+    statBoosts: {},
+  },
+  {
+    name: "Raw silver",
+    price: 25,
+    statBoosts: {},
+  },
+  {
+    name: "Raw copper",
+    price: 20,
+    statBoosts: {},
   }
 ];
+
+
+
 console.log("✅ Imports are loaded", imports);
