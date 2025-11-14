dbc541ba23b36dd46b8c8b5adf15e026d3146d8e
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-11-13 19:54:03 -0700

    Update spoils.js

--- data/spoils.js
+++ data/spoils.js
@@ -2,42 +2,15 @@ export const battleSpoils = [
   /////////////////
   //    TROOPS   //
   /////////////////
-  {
-    name: "Crossbows",
-    price: 8,
-    statBoosts: {troops: 1},
-  },
-  {
-    name: "Swords",
-    price: 10,
-    statBoosts: {troops: 2},
-  },
+  
   /////////////////
   //     GOLD    //
   /////////////////
   {
-    name: "Raw gold",
-    price: 30,
+    name: "Spare coins",
+    price: 10,
     statBoosts: {},
   },
-  {
-    name: "Raw silver",
-    price: 25,
-    statBoosts: {},
-  },
-  {
-    name: "Raw copper",
-    price: 20,
-    statBoosts: {},
-  },
-  {
-    name: "Jeweled goblet",
-    price: 40,
-    statBoosts: {},
-  },
-
-    
-  
 ];
 
 
