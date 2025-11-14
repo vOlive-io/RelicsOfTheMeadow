ae3f135cbaebc091545d80ecc763f1f18b9d0018
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-11-13 19:52:30 -0700

    Added items (more)

--- data/importItems.js
+++ data/importItems.js
@@ -22,9 +22,44 @@ export const importItems = [
     price: 5,
     statBoosts: {happiness: 5},
   },
+  {
+    name: "Concert Supplies",
+    price: 10,
+    statBoosts: {happiness: 10},
+  },
+  {
+    name: "Fireworks",
+    price: 12,
+    statBoosts: {happiness: 12},
+  },
+  {
+    name: "Luxury Goods",
+    price: 15,
+    statBoosts: {happiness: 15},
+  },
+  {
+    name: "Animals",
+    price: 20,
+    statBoosts: {happiness: 20}, 
+  },
+  {
+    name: "Town Square Renovation Supplies",
+    price: 25,
+    statBoosts: {happiness: 25},
+  },
+    {
+    name: "Festival Supplies",
+    price: 25,
+    statBoosts: {happiness: 25},
+  },
   /////////////////
   //  PROTECTION //
   /////////////////
+  {
+    name: "Shields",
+    price: 2,
+    statBoosts: {protection: 1},
+  },
   {
     name: "Wooden Fence Supplies",
     price: 3,
@@ -40,6 +75,16 @@ export const importItems = [
     price: 5,
     statBoosts: {protection: 3},
   },
+  {   
+    name: "Guard Tower Supplies",
+    price: 7,
+    statBoosts: {protection: 4},
+  },
+  {   
+    name: "Castle Wall Supplies",
+    price: 10,
+    statBoosts: {protection: 5},
+  },  
   /////////////////
   //    TROOPS   //
   /////////////////
@@ -54,9 +99,14 @@ export const importItems = [
     statBoosts: {troops: 2},
   },
    {
-    name: "Arrows Rockets",
+    name: "Rocket Arrows",
     price: 15,
     statBoosts: {troops: 3},
+  },
+  {
+    name: "Armored cavalry",
+    price: 15,
+    statBoosts: {troops: 5},
   },
   {
     name: "Catapults",
@@ -68,13 +118,28 @@ export const importItems = [
     price: 20,
     statBoosts: {troops: 5},
   },
+  {
+    name: "War Elephants",
+    price: 30,
+    statBoosts: {troops: 5},
+  },
   /////////////////
   //    ENERGY   //
   /////////////////
   {
-    name: "Energy Drink",
+    name: "Basic Energy Drink",
     price: 3,
     statBoosts: {energy: 5},
+  },
+  {
+    name: "Coffee",
+    price: 5,
+    statBoosts: {energy: 10},
+  },
+  {
+    name: "Premium Energy Drink",
+    price: 8,
+    statBoosts: {energy: 15},
   },
   /////////////////
   //     GOLD    //
@@ -93,7 +158,12 @@ export const importItems = [
     name: "Raw copper",
     price: 20,
     statBoosts: {},
-  }
+  },
+  {
+    name: "Jeweled goblet",
+    price: 40,
+    statBoosts: {},
+  },
 ];
 
 
---
+++ data/spoils.js
@@ -0,0 +1,45 @@
+export const battleSpoils = [
+  /////////////////
+  //    TROOPS   //
+  /////////////////
+  {
+    name: "Crossbows",
+    price: 8,
+    statBoosts: {troops: 1},
+  },
+  {
+    name: "Swords",
+    price: 10,
+    statBoosts: {troops: 2},
+  },
+  /////////////////
+  //     GOLD    //
+  /////////////////
+  {
+    name: "Raw gold",
+    price: 30,
+    statBoosts: {},
+  },
+  {
+    name: "Raw silver",
+    price: 25,
+    statBoosts: {},
+  },
+  {
+    name: "Raw copper",
+    price: 20,
+    statBoosts: {},
+  },
+  {
+    name: "Jeweled goblet",
+    price: 40,
+    statBoosts: {},
+  },
+
+    
+  
+];
+
+
+
+console.log("✅ Battle spoils are loaded", battleSpoils);
